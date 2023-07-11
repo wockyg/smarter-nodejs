6fb9de8407d93effb9a01740fb03d2b6785c0a3b
Warren <wmcclure1016@gmail.com>
2023-07-10 23:50:08 -0400

    put back env vars

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,14 @@
 module.exports = {
-  HOST: "209.182.208.106",
-  PORT: "3306",
-  USER: "define20_admin",
-  PASSWORD: "0SP]UY4*}]pG",
-  DB: "define20_smarter2",
+  HOST: process.env.HOST,
+  USER: process.env.USER,
+  PASSWORD: process.env.PASSWORD,
+  DB: process.env.DB,
   dialect: "mysql",
   timezone: "-04:00",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 };
--- app/models/index.js
+++ app/models/index.js
@@ -5,6 +5,13 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
   timezone: dbConfig.timezone,
+
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle
+  }
 });
 
 const db = {};
