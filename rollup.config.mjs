819ee7bdc4c65369eeb13467e801bdc337d67933
Warren <wmcclure1016@gmail.com>
2023-07-11 16:17:23 -0400

    rollup back working

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,8 @@
 import run from "@rollup/plugin-run";
+// import { nodeResolve } from '@rollup/plugin-node-resolve';
+// import commonjs from '@rollup/plugin-commonjs';
+// import json from '@rollup/plugin-json';
+
 
 export default {
   input: "server.js",
