99bb4bdf1f577abcdfed989c1030eb80e9ea4b94
Warren <wmcclure1016@gmail.com>
2023-07-10 23:28:02 -0400

    no port

--- app/models/index.js
+++ app/models/index.js
@@ -3,7 +3,6 @@ const dbConfig = require("../config/db.config.js");
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
-  port: dbConfig.PORT,
   dialect: dbConfig.dialect,
   timezone: dbConfig.timezone,
 
