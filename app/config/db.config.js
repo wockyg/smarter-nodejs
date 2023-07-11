9ebd3f6b789a67bbed268b8acf0e2b5249151006
Warren <wmcclure1016@gmail.com>
2023-07-10 23:35:37 -0400

    no pool

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,15 +1,9 @@
 module.exports = {
-  HOST: "ec2-52-41-36-82.us-west-2.compute.amazonaws.com",
+  HOST: "209.182.208.106",
   PORT: "3306",
   USER: "define20_admin",
   PASSWORD: "0SP]UY4*}]pG",
   DB: "define20_smarter2",
   dialect: "mysql",
   timezone: "-04:00",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
 };
