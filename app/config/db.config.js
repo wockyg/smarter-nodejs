c70aff0322d743a1e8421cdd4530b9a48fbc7d62
Warren <wmcclure1016@gmail.com>
2023-07-10 22:50:27 -0400

    no more env var

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  HOST: process.env.HOST,
-  USER: process.env.USER,
-  PASSWORD: process.env.PASSWORD,
-  DB: process.env.DB,
-  // PORT: process.env.PORT,
+  HOST: "209.182.208.106",
+  PORT: "3306",
+  USER: "define20_admin",
+  PASSWORD: "0SP]UY4*}]pG",
+  DB: "define20_smarter2",
   dialect: "mysql",
   timezone: "-04:00",
   pool: {
