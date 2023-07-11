929b634ef261ab0ebec942d2ae5826571fb5be9e
Warren <wmcclure1016@gmail.com>
2023-07-10 21:47:42 -0400

    comment

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ require("./app/routes/fceppdBillingView.routes")(app);
 
 require("./app/routes/lookup_UsState.routes")(app);
 
-const path = 'http://localhost:3000';
+// const path = 'http://localhost:3000';
 
 // D_CCWorkedFUEmail Task (6:00AM) (0 6 * * *)
 // schedule.scheduleJob('0 6 * * *', function(){
