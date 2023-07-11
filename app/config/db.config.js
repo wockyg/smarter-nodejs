5db4bef1ec01c96b11da199f86389c9ee8c2035d
Warren <wmcclure1016@gmail.com>
2023-07-10 22:30:01 -0400

    noport

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -3,7 +3,7 @@ module.exports = {
   USER: process.env.USER,
   PASSWORD: process.env.PASSWORD,
   DB: process.env.DB,
-  PORT: process.env.PORT,
+  // PORT: process.env.PORT,
   dialect: "mysql",
   timezone: "-04:00",
   pool: {
