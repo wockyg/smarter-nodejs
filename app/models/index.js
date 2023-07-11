914f845a56502ab0451f32a764d9ae29f807fc4b
Warren <wmcclure1016@gmail.com>
2023-07-10 23:37:00 -0400

    nopooool

--- app/models/index.js
+++ app/models/index.js
@@ -5,13 +5,6 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
   timezone: dbConfig.timezone,
-
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
-  }
 });
 
 const db = {};
