69cd35962613c9f9208c222103cf5b735af6248b
Warren <wmcclure1016@gmail.com>
2023-07-10 22:54:53 -0400

    no pw

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
   HOST: "209.182.208.106",
   PORT: "3306",
   USER: "define20_admin",
-  PASSWORD: "0SP]UY4*}]pG",
+  PASSWORD: "",
   DB: "define20_smarter2",
   dialect: "mysql",
   timezone: "-04:00",
